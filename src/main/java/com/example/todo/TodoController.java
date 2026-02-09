a03740989eb8fc92a67853de7bc51273603b686c
202510-aw-kudo <202510_aw_kudo@x-ide.sakura.ne.jp>
2026-02-09 12:21:56 +0900

    @RequestParamでフォームデータを受け取る ( recevoir les donnes du formulaire avec @RequestParam)

--- src/main/java/com/example/todo/TodoController.java
+++ src/main/java/com/example/todo/TodoController.java
@@ -7,19 +7,19 @@ import org.springframework.web.bind.annotation.PathVariable;
 @Controller
 public class TodoController {
 
-  // Display the todo list page.
+  // Affiche la page de la liste des todos.
   @GetMapping("/todos")
   public String listTodos() {
     return "todo/list";
   }
 
-  // Display the new todo creation page.
+  // Affiche la page de création d'un nouveau todo.
   @GetMapping("/todos/new")
   public String newTodo() {
     return "todo/new";
   }
 
-  // Display the todo detail page for the given id.
+  // Affiche la page de détail du todo pour l'id donné.
   @GetMapping("/todos/{id}")
   public String showTodo(@PathVariable("id") long id) {
     return "todo/detail";
