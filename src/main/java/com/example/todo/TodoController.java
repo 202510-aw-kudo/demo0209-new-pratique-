2672966143ea9b6e2a6aa19d76cd159817b97b96
202510-aw-kudo <202510_aw_kudo@x-ide.sakura.ne.jp>
2026-02-09 11:59:51 +0900

    画面遷移の実装 ( Implémentation de la transition d'écran)

--- src/main/java/com/example/todo/TodoController.java
+++ src/main/java/com/example/todo/TodoController.java
@@ -10,18 +10,18 @@ public class TodoController {
   // Display the todo list page.
   @GetMapping("/todos")
   public String listTodos() {
-    return "todos/list";
+    return "todo/list";
   }
 
   // Display the new todo creation page.
   @GetMapping("/todos/new")
   public String newTodo() {
-    return "todos/new";
+    return "todo/new";
   }
 
   // Display the todo detail page for the given id.
   @GetMapping("/todos/{id}")
   public String showTodo(@PathVariable("id") long id) {
-    return "todos/detail";
+    return "todo/detail";
   }
 }
